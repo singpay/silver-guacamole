df6c9e570e73da465b7493217ef7184f80a8b29e
singpay <107242688+singpay@users.noreply.github.com>
2023-08-22 18:31:07 +0700

    Initialize examples/uniswap-v2

---
+++ hardhat.config.ts
@@ -0,0 +1,108 @@
+import 'dotenv/config';
+import {HardhatUserConfig} from 'hardhat/types';
+import 'hardhat-deploy';
+import '@nomiclabs/hardhat-ethers';
+import 'hardhat-gas-reporter';
+import '@typechain/hardhat';
+import 'solidity-coverage';
+import 'hardhat-deploy-tenderly';
+import {node_url, accounts, addForkConfiguration} from './utils/network';
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.9',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 2000,
+          },
+        },
+      },
+    ],
+  },
+  namedAccounts: {
+    deployer: 0,
+    simpleERC20Beneficiary: 1,
+    uniswap_deployer: 'deployer',
+    uniswap_feeToSetter: 2,
+  },
+  networks: addForkConfiguration({
+    hardhat: {
+      initialBaseFeePerGas: 0, // to fix : https://github.com/sc-forks/solidity-coverage/issues/652, see https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136
+    },
+    localhost: {
+      url: node_url('localhost'),
+      accounts: accounts(),
+    },
+    staging: {
+      url: node_url('rinkeby'),
+      accounts: accounts('rinkeby'),
+    },
+    production: {
+      url: node_url('mainnet'),
+      accounts: accounts('mainnet'),
+    },
+    mainnet: {
+      url: node_url('mainnet'),
+      accounts: accounts('mainnet'),
+    },
+    rinkeby: {
+      url: node_url('rinkeby'),
+      accounts: accounts('rinkeby'),
+    },
+    kovan: {
+      url: node_url('kovan'),
+      accounts: accounts('kovan'),
+    },
+    goerli: {
+      url: node_url('goerli'),
+      accounts: accounts('goerli'),
+    },
+  }),
+  paths: {
+    sources: 'src',
+  },
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 100,
+    enabled: process.env.REPORT_GAS ? true : false,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    maxMethodDiff: 10,
+  },
+  typechain: {
+    outDir: 'typechain',
+    target: 'ethers-v5',
+  },
+  mocha: {
+    timeout: 0,
+  },
+  external: {
+    deployments: process.env.HARDHAT_FORK
+      ? {
+          // process.env.HARDHAT_FORK will specify the network that the fork is made from.
+          // these lines allow it to fetch the deployments from the network being forked from both for node and deploy task
+          hardhat: ['deployments/' + process.env.HARDHAT_FORK],
+          localhost: ['deployments/' + process.env.HARDHAT_FORK],
+        }
+      : undefined,
+    contracts: [
+      {
+        artifacts: [
+          'node_modules/hardhat-deploy-uniswap-v2/export/artifacts',
+          'node_modules/@uniswap/v2-core/build',
+          'node_modules/@uniswap/v2-periphery/build',
+        ],
+        deploy: 'node_modules/hardhat-deploy-uniswap-v2/export/deploy',
+      },
+    ],
+  },
+
+  tenderly: {
+    project: 'template-ethereum-contracts',
+    username: process.env.TENDERLY_USERNAME as string,
+  },
+};
+
+export default config;
